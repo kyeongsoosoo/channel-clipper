5f3b5cc17ab64274450998028f25486299629718
kyeongsoosoo <shakramon@naver.com>
2022-06-03 14:12:17 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+      'airbnb-typescript/base'
+    ],
+    plugins: [
+      'import',
+    ],
+    parserOptions: {
+      project: './tsconfig.lint.json',
+    },  
+  }
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+  it('case', () => {
+    expect(1).toBe(1);
+  });
+});
