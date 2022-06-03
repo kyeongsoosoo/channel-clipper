d739f084feb3137c0163b5efd910f3593db94209
kyeongsoosoo <shakramon@naver.com>
2022-06-03 15:56:52 +0900

    env: typescript 설정

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-const express = require('express'); //express를 설치했기 때문에 가져올 수 있다.
+/* External dependencies */
+import 'dotenv/config';
+import express from 'express';
+import { WebhookResponse } from './types';
+
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded( { extended : false } ));
 
-app.listen(3000);
+
+app.post('/', 
+  (res: WebhookResponse) => {
+    try {
+      const { body } = res;
+      
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+
+app.listen(PORT);
